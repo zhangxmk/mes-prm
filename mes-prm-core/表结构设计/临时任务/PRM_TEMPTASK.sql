851d8943b9ac93d9a847ed9ce47eb0dfa49288a2
guojunf <guojunf@yonyou.com>
2018-03-20 15:46:35 +0800

    临时任务
    Change-Id: I935446ca6b52441d4105d47213e4ee022c86e5c3
    Signed-off-by: guojunf <guojunf@yonyou.com>

---
+++ mes-prm-core/表结构设计/临时任务/PRM_TEMPTASK.sql
@@ -0,0 +1,25 @@
+-- Create table
+create table PRM_TEMPTASK
+(
+  id             CHAR(36) primary key,
+  cdeptid        VARCHAR2(36),
+  vdeptname      VARCHAR2(50),
+  cqcprojectid   VARCHAR2(36),
+  vqcprojectcode VARCHAR2(50),
+  vqcprojectname VARCHAR2(50),
+  cstationid     VARCHAR2(36),
+  vstationname   VARCHAR2(50),
+  tqctime        TIMESTAMP(6),
+  tenantid       VARCHAR2(36),
+  sysid          VARCHAR2(50),
+  orgid          VARCHAR2(36),
+  orgid_name     VARCHAR2(50),
+  ts             TIMESTAMP(6) not null,
+  dr             INTEGER not null,
+  creator        VARCHAR2(36),
+  creator_name   VARCHAR2(50),
+  creationtime   TIMESTAMP(6),
+  modifier       VARCHAR2(36),
+  modifier_name  VARCHAR2(50),
+  modifiedtime   TIMESTAMP(6)
+)
---
+++ mes-prm-core/表结构设计/临时任务/PRM_TEMPTASK_B.sql
@@ -0,0 +1,22 @@
+-- Create table
+create table PRM_TEMPTASK_B
+(
+  id            CHAR(36) primary key,
+  vrowno        VARCHAR2(36),
+  vprjcontent   VARCHAR2(100),
+  vprjcriterion VARCHAR2(1000),
+  vqcvalue      VARCHAR2(50),
+  pk_temptask   CHAR(36) not null,
+  tenantid      VARCHAR2(36),
+  sysid         VARCHAR2(50),
+  orgid         VARCHAR2(36),
+  orgid_name    VARCHAR2(50),
+  ts            TIMESTAMP(6),
+  dr            INTEGER,
+  creator       VARCHAR2(36),
+  creator_name  VARCHAR2(50),
+  creationtime  TIMESTAMP(6),
+  modifier      VARCHAR2(36),
+  modifier_name VARCHAR2(50),
+  modifiedtime  TIMESTAMP(6)
+)
