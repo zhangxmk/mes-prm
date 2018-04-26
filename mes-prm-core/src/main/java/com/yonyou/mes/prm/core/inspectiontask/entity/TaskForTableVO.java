57a5b86286bf69967218a4e692e1acea9a0a9400
wangyang <wangyang12@yonyou.com>
2018-04-26 20:30:43 +0800

    巡检完成情况统计表

---
+++ mes-prm-core/src/main/java/com/yonyou/mes/prm/core/inspectiontask/entity/TaskForTableVO.java
@@ -0,0 +1,351 @@
+package com.yonyou.mes.prm.core.inspectiontask.entity;
+
+import java.sql.Timestamp;
+
+import com.yonyou.me.entity.MeSuperVO;
+
+public class TaskForTableVO extends MeSuperVO{
+	
+	private String postid;
+	private String postid_name;
+	private String team;
+	private String team_name;
+	private String shift;
+	private String shift_name;
+	private String orgid;
+	private String orgid_name;
+	private String plancount;
+	private String unfinishcount;
+
+
+
+	public String getPlancount() {
+		return plancount;
+	}
+
+
+	public void setPlancount(String plancount) {
+		this.plancount = plancount;
+	}
+
+
+	public String getUnfinishcount() {
+		return unfinishcount;
+	}
+
+
+	public void setUnfinishcount(String unfinishcount) {
+		this.unfinishcount = unfinishcount;
+	}
+
+
+
+
+	public String getPostid() {
+		return postid;
+	}
+
+
+	public void setPostid(String postid) {
+		this.postid = postid;
+	}
+
+
+	public String getPostid_name() {
+		return postid_name;
+	}
+
+
+	public void setPostid_name(String postid_name) {
+		this.postid_name = postid_name;
+	}
+
+
+
+
+
+
+	
+	public String getTeam() {
+		return team;
+	}
+
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+
+	public String getTeam_name() {
+		return team_name;
+	}
+
+
+	public void setTeam_name(String team_name) {
+		this.team_name = team_name;
+	}
+
+
+	public String getShift() {
+		return shift;
+	}
+
+
+	public void setShift(String shift) {
+		this.shift = shift;
+	}
+
+
+	public String getShift_name() {
+		return shift_name;
+	}
+
+
+	public void setShift_name(String shift_name) {
+		this.shift_name = shift_name;
+	}
+
+
+
+
+
+	public String getOrgid() {
+		return this.orgid;
+	}
+
+	public void setOrgid(String orgid) {
+		this.orgid = orgid;
+	}
+
+
+	public String getOrgid_name() {
+		return this.orgid_name;
+	}
+
+	public void setOrgid_name(String orgid_name) {
+		this.orgid_name = orgid_name;
+	}
+
+
+
+
+	
+
+	@Override
+	public String getName() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public void setName(String name) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public String getCode() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public String getOriginalid() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public String getParentid() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+
+	@Override
+	public String getTableName() {
+		// TODO 自动生成的方法存根
+		return "PRM_TASK";
+	}
+
+
+
+	@Override
+	public void setCode(String code) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setOriginalid(String originalid) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setParentid(String parentid) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public Timestamp getCreationtime() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public String getCreator() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public String getCreator_name() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public Integer getDr() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public String getId() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public Timestamp getModifiedtime() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public String getModifier() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public String getModifier_name() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public String getSysid() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public String getTenantid() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public Timestamp getTs() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+
+	@Override
+	public void setCreationtime(Timestamp arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setCreator(String arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setCreator_name(String arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setDr(Integer arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setId(String arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setModifiedtime(Timestamp arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setModifier(String arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setModifier_name(String arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setSysid(String arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setTenantid(String arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+
+	@Override
+	public void setTs(Timestamp arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+}
